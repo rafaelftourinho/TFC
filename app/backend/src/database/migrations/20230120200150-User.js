27c3edf8c81b244a0d498415abd40ffd199f6746
rafaelftourinho <rafaelftourinho@gmail.com>
2023-01-20 17:03:13 -0300

    :recycle: criacao de migration do User

---
+++ app/backend/src/database/migrations/20230120200150-User.js
@@ -0,0 +1,11 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Users', {
+      id: { type: Sequelize.INTEGER, allowNull: false, primaryKey: true, autoIncrement: true, },
+      username: { type: Sequelize.INTEGER, allowNull: false, },
+      role: { type: Sequelize.INTEGER, allowNull: false, },
+      email: { type: Sequelize.INTEGER, allowNull: false, },
+      password: { type: Sequelize.INTEGER, allowNull: false, } });
+    },
+  down: async (queryInterface) => { await queryInterface.dropTable('Users'); }, };
