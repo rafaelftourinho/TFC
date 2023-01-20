eab6acf59af21aeb3aeb5b58df6144589f556738
rafaelftourinho <rafaelftourinho@gmail.com>
2023-01-20 17:00:12 -0300

    :recycle: criação de dockerfile front e back, criação da model User

---
+++ app/backend/src/database/models/User.ts
@@ -0,0 +1,20 @@
+import { Model, INTEGER, STRING } from 'sequelize'; import db from '.';
+
+class User extends Model {
+  declare id: number;
+  declare username: string;
+  declare role: string;
+  declare email: string;
+  declare password: string;
+}
+
+User.init(
+  {
+    id: { type: INTEGER, allowNull: false, primaryKey: true, autoIncrement: true },
+    username: { type: STRING(30), allowNull: false },
+    role: { type: STRING(30), allowNull: false },
+    email: { type: STRING(30), allowNull: false },
+    password: { type: STRING(30), allowNull: false },
+  },
+  { underscored: true, sequelize: db, modelName: 'users', timestamps: false },
+); export default User;
