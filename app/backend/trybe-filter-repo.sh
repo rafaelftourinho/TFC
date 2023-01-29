4fafa04aa4833c57652d377f1ee6ea8f32df358a
rafaelftourinho <rafaelftourinho@gmail.com>
2023-01-29 20:13:16 -0300

    add trybe-filter-repo.sh

---
+++ app/backend/trybe-filter-repo.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+### GIT FILTER-REPO ###
+
+## NÃO EXECUTE ESSE SCRIPT DIRETAMENTE
+## Esse script foi feito para uso do
+## script 'trybe-publisher' fornecido
+## pela Trybe.
+
+[[ $# == 1 ]] && \
+[[ $1 == "trybe-security-parameter" ]] && \
+git filter-repo \
+    --path .trybe \
+    --path .github \
+    --path .vscode \
+    --path trybe.yml \
+    --path trybe-filter-repo.sh \
+    --path README.md \
+    --path __tests__ \
+    --path jest.config.js \
+    --path data-testid.md \
+    --path assets \
+    --invert-paths --force --quiet
